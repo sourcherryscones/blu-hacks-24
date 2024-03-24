4166259d7b2199bdc30322d752e86248398684e4
sourcherryscones <purva.marfatia@gmail.com>
2024-03-24 14:43:23 -0700

    login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    let username = "";
+    let password = "";
+
+    function login(){
+        fetch('', {
+            method:"POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username,
+                password
+            })
+        })
+    }
+
+</script>
+
+<input class="input input-bordered" name="username" placeholder="username" bind:value={username}>
+<input type="password" class="input input-bordered" name="password" placeholder="password" bind:value={password}>
+<button on:click={login}>Log In</button>

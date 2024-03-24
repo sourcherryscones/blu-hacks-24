16188fc259cb9abb3406c7acb5c211a5ce0b3059
Shine Chang <shine00chang@gmail.com>
2024-03-24 13:21:20 -0700

    (fix) style

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,23 +1,25 @@
 <script>
-    let username = "";
-    let password = "";
+  let username = "";
+  let password = "";
 
-    function signup(){
-        fetch(/*whatever route*/, {
-            method:"POST",
-            headers:{
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                username,
-                password
-            })
-        })
-    }
-
+  function signup(){
+    fetch('/signup', {
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    })
+  }
 </script>
 
-<input name="username" bind:value={username}>
-<input name="password" bind:value={password}>
-<button on:click={signup}>Sign up</button>
+<div class="flex flex-col w-48 gap-2 m-4">
+  <input class="input input-bordered" name="username" bind:value={username}>
+  <input class="input input-bordered" name="password" bind:value={password}>
+
+  <button class="btn" on:click={signup}>Sign up</button>
+</div>
 
